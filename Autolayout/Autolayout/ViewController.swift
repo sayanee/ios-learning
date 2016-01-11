b92c57df6a2ef312223cc8970e67b7481d526d6d
Sayanee <sayanee@gmail.com>
2016-01-11 14:00:22 +0800

    toggle security field

--- Autolayout/Autolayout/ViewController.swift
+++ Autolayout/Autolayout/ViewController.swift
@@ -11,6 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
    
-
+    @IBOutlet weak var loginField: UIButton!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    var secure: Bool = false {
+        didSet { updateUI() }
+    }
+    
+    private func updateUI() {
+        passwordField.secureTextEntry = false
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+    }
+    
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
 }
 
