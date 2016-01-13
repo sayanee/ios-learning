68c03e68bb1f6c6c8f5960b41e34709b5aa55e2e
Sayanee <sayanee@gmail.com>
2016-01-13 14:21:03 +0800

    load plain tweets with hastag - compatible with swift 2.0

---
+++ Smashtag/Smashtag/User.swift
@@ -0,0 +1,74 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+// container to hold data about a Twitter user
+
+public struct User: CustomStringConvertible
+{
+    public var screenName: String = ""
+    public var name: String = ""
+    public var profileImageURL: NSURL? = nil
+    public var verified: Bool
+    public var id: String!
+    
+    public var description: String { let v = verified ? " ✅" : ""; return "@\(screenName) (\(name))\(v)" }
+    
+    // MARK: - Private Implementation
+    
+    init?(data: NSDictionary?) {
+        let name = data?.valueForKeyPath(TwitterKey.Name) as? String
+        let screenName = data?.valueForKeyPath(TwitterKey.ScreenName) as? String
+        if name != nil && screenName != nil {
+            self.name = name!
+            self.screenName = screenName!
+        }
+        
+        self.id = data?.valueForKeyPath(TwitterKey.ID) as? String
+        
+        if let verified = data?.valueForKeyPath(TwitterKey.Verified)?.boolValue {
+            self.verified = verified
+        } else {
+            self.verified = false
+        }
+        
+        if let urlString = data?.valueForKeyPath(TwitterKey.ProfileImageURL) as? String {
+            self.profileImageURL = NSURL(string: urlString)
+        } else {
+            self.profileImageURL = nil
+        }
+    }
+    
+    var asPropertyList: AnyObject {
+        var dictionary = Dictionary<String,String>()
+        dictionary[TwitterKey.Name] = self.name
+        dictionary[TwitterKey.ScreenName] = self.screenName
+        dictionary[TwitterKey.ID] = self.id
+        dictionary[TwitterKey.Verified] = verified ? "YES" : "NO"
+        dictionary[TwitterKey.ProfileImageURL] = profileImageURL?.absoluteString
+        return dictionary
+    }
+    
+    
+    init() {
+        screenName = "Unknown"
+        name = "Unknown"
+        profileImageURL = nil
+        verified = false
+        id = nil
+    }
+    
+    struct TwitterKey {
+        static let Name = "name"
+        static let ScreenName = "screen_name"
+        static let ID = "id_str"
+        static let Verified = "verified"
+        static let ProfileImageURL = "profile_image_url"
+    }
+}
