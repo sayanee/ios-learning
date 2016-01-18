658370a454abb1306da9f622150f6d2236676738
Sayanee <sayanee@gmail.com>
2016-01-18 09:57:18 +0800

    add dropit project

---
+++ Dropit/DropitViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DropitViewController.swift
+//  Dropit
+//
+//  Created by Sayanee Basu on 15/1/16.
+//  Copyright © 2016 Sayanee Basu. All rights reserved.
+//
+
+import UIKit
+
+class DropitViewController: UIViewController {
+
+    @IBAction func drop(sender: UITapGestureRecognizer) {
+        drop()
+    }
+    
+    @IBOutlet weak var gameView: UIView!
+    
+    var dropsPerRow = 10
+    
+    var dropSize: CGSize {
+        let size = gameView.bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height:  size)
+    }
+    
+    func drop() {
+        var frame = CGRect(origin: CGPointZero, size: dropSize)
+        print(CGFloat.random(dropsPerRow))
+        frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
+        
+        let dropView = UIView(frame: frame)
+        dropView.backgroundColor = UIColor.random
+        
+        gameView.addSubview(dropView)
+    }
+}
+
+private extension CGFloat {
+    static func random(max: Int) -> CGFloat {
+        return CGFloat(arc4random() % UInt32(max))
+    }
+}
+
+private extension UIColor {
+    class var random: UIColor {
+        switch arc4random()%5 {
+        case 0: return UIColor.greenColor()
+        case 1: return UIColor.blueColor()
+        case 2: return UIColor.orangeColor()
+        case 3: return UIColor.redColor()
+        case 4: return UIColor.purpleColor()
+        default: return UIColor.blackColor()
+        }
+    }
+}
