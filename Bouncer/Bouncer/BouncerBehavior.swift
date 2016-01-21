ab07713952c881d3c077ffe88876123975e0682a
Sayanee <sayanee@gmail.com>
2016-01-21 21:51:55 +0800

    Bouncer: implement elasticity settings

--- Bouncer/Bouncer/BouncerBehavior.swift
+++ Bouncer/Bouncer/BouncerBehavior.swift
@@ -21,9 +21,12 @@ class BouncerBehavior: UIDynamicBehavior {
     lazy var blockBehavior: UIDynamicItemBehavior = {
         let lazilyCreatedBlockBehavior = UIDynamicItemBehavior()
         lazilyCreatedBlockBehavior.allowsRotation = true
-        lazilyCreatedBlockBehavior.elasticity = 0.75
+        lazilyCreatedBlockBehavior.elasticity = CGFloat(NSUserDefaults.standardUserDefaults().doubleForKey("BouncerBehavior.Elasticity"))
         lazilyCreatedBlockBehavior.friction = 0
         lazilyCreatedBlockBehavior.resistance = 0
+        NSNotificationCenter.defaultCenter().addObserverForName(NSUserDefaultsDidChangeNotification, object: nil, queue: nil) { (notification) -> Void in
+            lazilyCreatedBlockBehavior.elasticity = CGFloat(NSUserDefaults.standardUserDefaults().doubleForKey("BouncerBehavior.Elasticity"))
+        }
         
         return lazilyCreatedBlockBehavior
     }()
